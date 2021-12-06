7fa72c5faef3f3ab1fe4df70a2e83d7102e07a95
Ethan George <etgeorge@davidson.edu>
2021-12-06 17:31:41 +0100

    Added getURI method, since you can only copy song links from the mobile app. Thus, users will search songs via song link and we will get the URI from the URL to query the API

--- app/src/main/java/com/ait/songifyait/MainActivity.kt
+++ app/src/main/java/com/ait/songifyait/MainActivity.kt
@@ -2,12 +2,27 @@ package com.ait.songifyait
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ait.songifyait.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         val Demo = "Did this push work?"
+
+        binding.tvURI.text = getURI(
+            "https://open.spotify.com/track/4laGLspUjGjh1O0AZ2tEQk?si=a22a7892d6344620")
+    }
+
+
+    fun getURI(url : String): String {
+        var trackURI = ""
+        for (i in 31..52) {
+                trackURI += url[i]
+        }
+        return trackURI
     }
 }
