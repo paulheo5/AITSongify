5aaf36c520b01ac346ea2fc3fe3b523c56709e59
Ethan George <etgeorge@davidson.edu>
2021-12-07 22:40:32 +0100

    Trying to fix merge stuff

--- app/src/main/java/com/ait/songifyait/Compare.kt
+++ app/src/main/java/com/ait/songifyait/Compare.kt
@@ -12,6 +12,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import java.lang.Math.abs
 import retrofit2.converter.gson.GsonConverterFactory
 
 class Compare : AppCompatActivity() {
@@ -22,6 +23,43 @@ class Compare : AppCompatActivity() {
         compareBinding = ActivityCompareBinding.inflate(layoutInflater)
         setContentView(compareBinding.root)
         getAuthorization()
+        var Points = 0
+
+//        energy
+//        danceAbility1
+//        instramentalness1
+//        key
+//        loudless
+//        timesignature1
+//        valence1
+//        tempo1
+//        fun comparision() {
+//            if (abs(danceAbility1 - danceAbility2) < .25) {
+//                Points = +1
+//            }
+//            if (abs(energy1 - energy2) < .25) {
+//                Points = +1
+//            }
+//
+//            if (abs(instramentalness1 - instramentalness2) < .2) {
+//                Points = +1
+//            }
+//            if (abs(key1 - key2) < 3) {
+//                Points = +1
+//            }
+//            if (abs(loudness1 - loudless2) < 20) {
+//                Points = +1
+//            }
+//            if (abs(timesignature1 - timesignature2) < 3) {
+//                Points = +1
+//            }
+//            if (abs(valence1 - valence2) < .25) {
+//                Points = +1
+//            }
+//            if (abs(tempo1 - tempo2) < 34) {
+//                Points = +1
+//            }
+//        }
 
     }
 
